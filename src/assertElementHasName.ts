e7e7563a980ba5b94c5f5c4555263be19c8603c2
Mohammad Ataei <mammadataei@gmail.com>
2022-06-04 11:16:39 +0430

    feat: add assertElementHasName

---
+++ src/assertElementHasName.ts
@@ -0,0 +1,36 @@
+import { computeAccessibleName } from 'dom-accessibility-api'
+
+function elementToString(element: HTMLElement) {
+  return `<${element.tagName.toLowerCase()}>`
+}
+
+export function assertElementHasName(
+  htmlElement: HTMLElement,
+  expectedName?: string | RegExp,
+) {
+  const actualName = computeAccessibleName(htmlElement)
+  const elementName = elementToString(htmlElement)
+
+  if (!expectedName) {
+    return {
+      pass: actualName !== '',
+      message: `Expected the ${elementName} element to have an accessible name.`,
+      negatedMessage: `Expected the ${elementName} element not to have any accessible names, but received "${actualName}".`,
+      expected: '',
+      received: actualName,
+    }
+  }
+
+  const pass =
+    expectedName instanceof RegExp
+      ? expectedName.test(actualName)
+      : actualName === expectedName
+
+  return {
+    pass,
+    message: `Expected the ${elementName} element to have accessible name "${expectedName}", but received "${actualName}".`,
+    negatedMessage: `Expected the ${elementName} element not to have accessible name "${expectedName}".`,
+    expected: expectedName,
+    received: actualName,
+  }
+}
