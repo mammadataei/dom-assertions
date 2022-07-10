bc818d5f413846cda25f00b021bcaf42c3acb336
Mohammad Ataei <mammadataei@gmail.com>
2022-07-10 15:41:48 +0430

    fix(cypress-dom): resolve ReactDom use createRoot error

--- packages/cypress-dom/cypress/support/component.ts
+++ packages/cypress-dom/cypress/support/component.ts
@@ -1,5 +1,8 @@
 import { mount } from 'cypress/react'
 import '@testing-library/cypress/add-commands'
+import { createRoot, Root } from 'react-dom/client'
+import type { ReactNode } from 'react'
+import type { MountOptions } from 'cypress/react/dist/mount'
 
 declare global {
   namespace Cypress {
@@ -9,4 +12,20 @@ declare global {
   }
 }
 
-Cypress.Commands.add('render', mount)
+Cypress.Commands.add('render', (jsx: ReactNode, options?: MountOptions) => {
+  let root: Root | undefined = undefined
+
+  return mount(jsx, {
+    ...options,
+    ReactDom: {
+      render: (_: unknown, rootEl: Element) => {
+        root = createRoot(rootEl)
+        root.render(jsx)
+      },
+      unmountComponentAtNode: () => {
+        root?.unmount()
+        return true
+      },
+    },
+  })
+})
