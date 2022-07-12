bfcfc9f5f64df1bc4d7712d16a2289ccbd552adc
Mohammad Ataei <mammadataei@gmail.com>
2022-07-12 19:57:24 +0430

    feat(cypress-dom): add have.errorMessage chainer

--- packages/cypress-dom/src/index.ts
+++ packages/cypress-dom/src/index.ts
@@ -4,24 +4,56 @@ declare global {
   namespace Cypress {
     interface Chainer<Subject> {
       /**
-       * Chai assertion that checks if a given element has any/expected description
+       * Assert that the given element has any/expected description
        * @param chainer
-       * @param expected (optional) expected data-test-id value
+       * @param expected (optional) expected description
        * @example
        *  cy.get('#id').should('have.description')
-       *  cy.get('#id').should('have.description', 'expected value')
+       *  cy.get('#id').should('have.description', 'expected description')
        */
-      (chainer: 'have.description', expected?: string): Chainable<Subject>
+      (
+        chainer: 'have.description',
+        expected?: string | RegExp,
+      ): Chainable<Subject>
 
       /**
        * Assert that the given element doesn't have any/expected description
        * @param chainer
-       * @param expected (optional) expected data-test-id value
+       * @param expected (optional) expected description
        * @example
        *  cy.get('#id').should('not.have.description')
-       *  cy.get('#id').should('not.have.description', 'expected value')
+       *  cy.get('#id').should('not.have.description', 'expected description')
        */
-      (chainer: 'not.have.description', expected?: string): Chainable<Subject>
+      (
+        chainer: 'not.have.description',
+        expected?: string | RegExp,
+      ): Chainable<Subject>
+
+      /**
+       * Asserts that the given element has any/expected error message
+       * @param chainer
+       * @param expected (optional) expected error message
+       * @example
+       *  cy.get('#id').should('not.have.errorMessage')
+       *  cy.get('#id').should('not.have.errorMessage', 'expected error message')
+       */
+      (
+        chainer: 'have.errorMessage',
+        expected?: string | RegExp,
+      ): Chainable<Subject>
+
+      /**
+       * Asserts that the given element doesn't have any/expected error message
+       * @param chainer
+       * @param expected (optional) expected error message
+       * @example
+       *  cy.get('#id').should('not.have.errorMessage')
+       *  cy.get('#id').should('not.have.errorMessage', 'expected error message')
+       */
+      (
+        chainer: 'not.have.errorMessage',
+        expected?: string | RegExp,
+      ): Chainable<Subject>
     }
   }
 }
