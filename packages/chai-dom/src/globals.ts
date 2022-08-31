3d16e7157b03cc6feaa864503c89384a3e14944d
Mohammad Ataei <mammadataei@gmail.com>
2022-08-31 19:38:15 +0430

    feat(chai-dom): add have.errorMessage

--- packages/chai-dom/src/globals.ts
+++ packages/chai-dom/src/globals.ts
@@ -11,6 +11,15 @@ declare global {
       description(expectedDescription?: string | RegExp): void
 
       /**
+       * Asserts that the given element has any/expected error message
+       * @param expected (optional) expected error message
+       * @example
+       *  expect($element).to.have.errorMessage()
+       *  expect($element).to.have.errorMessage('expected error message')
+       */
+      errorMessage(expected?: string | RegExp): void
+
+      /**
        * Asserts that the given element has any/expected accessible name
        * @see https://w3c.github.io/accname/
        * @param expected (optional) expected name
