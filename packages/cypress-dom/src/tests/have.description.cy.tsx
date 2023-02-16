3ee1739b3e624c578df492f753b358da685cca94
Mohammad Ataei <mammadataei@gmail.com>
2023-02-16 21:15:55 +0330

    test(cypress-dom): improve coverage for `have.description`

--- packages/cypress-dom/src/tests/have.description.cy.tsx
+++ packages/cypress-dom/src/tests/have.description.cy.tsx
@@ -11,10 +11,24 @@ it('asserts element has description', () => {
 
   cy.get('@input').should('have.description')
   cy.get('@input').should('have.description', 'Pick a strong password!')
+  cy.get('@input').should('not.have.description', 'Enter your email address!')
 
   cy.get('@input').should(($input) => {
     expect($input).to.have.description()
     expect($input).to.have.description('Pick a strong password!')
+    expect($input).not.to.have.description('Enter your email address!')
+
+    expect(() => {
+      expect($input).not.to.have.description('Pick a strong password!')
+    }).to.throw(
+      'Expected the <input> element not to have accessible description "Pick a strong password!"',
+    )
+
+    expect(() => {
+      expect($input).to.have.description('Enter your email address!')
+    }).to.throw(
+      'Expected the <input> element to have accessible description "Enter your email address!", but received "Pick a strong password!"',
+    )
   })
 })
 
@@ -34,5 +48,17 @@ it('asserts element does not have description', () => {
   cy.get('@input').should(($input) => {
     expect($input).not.to.have.description()
     expect($input).not.to.have.description('Pick a strong password!')
+
+    expect(() => {
+      expect($input).to.have.description()
+    }).to.throw(
+      'Expected the <input> element to have an accessible description',
+    )
+
+    expect(() => {
+      expect($input).to.have.description('Pick a strong password!')
+    }).to.throw(
+      'Expected the <input> element to have accessible description "Pick a strong password!", but received ""',
+    )
   })
 })
