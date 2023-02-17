00dc29c84e080ae6c53497028b9d463386789a58
Mohammad Ataei <mammadataei@gmail.com>
2023-02-17 21:59:01 +0330

    test(cypress-dom): improve coverage for `have.name`

--- packages/cypress-dom/src/tests/have.name.cy.tsx
+++ packages/cypress-dom/src/tests/have.name.cy.tsx
@@ -10,10 +10,24 @@ it('asserts element has name', () => {
 
   cy.get('@input').should('have.name')
   cy.get('@input').should('have.name', 'Email')
+  cy.get('@input').should('not.have.name', 'Password')
 
   cy.get('@input').should(($input) => {
     expect($input).to.have.name()
     expect($input).to.have.name('Email')
+    expect($input).not.to.have.name('Password')
+
+    expect(() => {
+      expect($input).not.to.have.name('Email')
+    }).to.throw(
+      'Expected the <input> element not to have accessible name "Email"',
+    )
+
+    expect(() => {
+      expect($input).to.have.name('Password')
+    }).to.throw(
+      'Expected the <input> element to have accessible name "Password", but received "Email"',
+    )
   })
 })
 
@@ -32,5 +46,15 @@ it('asserts element does not have name', () => {
   cy.get('@input').should(($input) => {
     expect($input).not.to.have.name()
     expect($input).not.to.have.name('Email')
+
+    expect(() => {
+      expect($input).to.have.name()
+    }).to.throw('Expected the <input> element to have an accessible name')
+
+    expect(() => {
+      expect($input).to.have.name('Email')
+    }).to.throw(
+      'Expected the <input> element to have accessible name "Email", but received ""',
+    )
   })
 })
